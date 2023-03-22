d50584c115d1661e3bed0b9940a12730a1afcf61
Francisco Dávila <fjhdavila@gmail.com>
2023-03-22 13:49:52 +0000

    Modificada barra de navegación

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import './Navbar.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-	return (
-		<div>
-			<ul>
-				<li>
-					{/* <NavLink className={(data) => {console.log(data)}} to='./'>Inicio</NavLink> */}
-					<NavLink
-						className={({ isActive }) => (isActive ? 'activo' : 'noactivo')}
-						to='./'
-					>
-						Inicio
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						className={({ isActive }) => (isActive ? 'activo' : 'noactivo')}
-						to='./about'
-					>
-						Acerca de
-					</NavLink>
-				</li>
-			</ul>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Esto es el Navbar</h1>
+      <ul>
+        <li>
+          {/* <NavLink className={(data) => {console.log(data)}} to='./'>Inicio</NavLink> */}
+          <NavLink
+            className={({ isActive }) => (isActive ? "activo" : "noactivo")}
+            to="./"
+          >
+            Inicio
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            className={({ isActive }) => (isActive ? "activo" : "noactivo")}
+            to="./about"
+          >
+            Acerca de
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
